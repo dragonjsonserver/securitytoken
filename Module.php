ae87022cad1e46b0b89250f3eae6299c26743312
Christoph Herrmann <developer@dragonprojects.de>
2013-03-28 14:21:50 +0100

    Erweiterung für den DragonJsonServer um API Namensräume durch Securitytokens zu sichern

---
+++ Module.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link http://dragonjsonserver.de/
+ * @copyright Copyright (c) 2012-2013 DragonProjects (http://dragonprojects.de/)
+ * @license http://license.dragonprojects.de/dragonjsonserver.txt New BSD License
+ * @author Christoph Herrmann <developer@dragonprojects.de>
+ * @package DragonJsonServerSecuritytoken
+ */
+
+namespace DragonJsonServerSecuritytoken;
+
+/**
+ * Klasse zur Initialisierung des Moduls
+ */
+class Module
+{
+    /**
+     * Gibt die Konfiguration des Moduls zurück
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @link http://dragonjsonserver.de/
+ * @copyright Copyright (c) 2012-2013 DragonProjects (http://dragonprojects.de/)
+ * @license http://license.dragonprojects.de/dragonjsonserver.txt New BSD License
+ * @author Christoph Herrmann <developer@dragonprojects.de>
+ * @package DragonJsonServerSecuritytoken
+ */
+
+/**
+ * @return array
+ */
+return [
+    'securitytokens' => [],
+    'eventlisteners' => [
+    	['DragonJsonServer\Service\Server', 'request', function (\DragonJsonServer\Event\Request $request) {
+    		$method = $request->getRequest()->getMethod();
+    		foreach ($request->getServiceManager()->get('Config')['securitytokens'] as $namespace => $securitytoken) {
+    			$namespace .= '.';
+	    		if (substr($method, 0, strlen($namespace)) != $namespace) {
+	    			continue;
+	    		}
+	    		if ($request->getRequest()->getParam('securitytoken') != $securitytoken) {
+	    			throw new \DragonJsonServer\Exception('incorrect securitytoken', ['namespace' => $namespace]);
+	    		} 
+    		}
+    	}],
+    	['DragonJsonServer\Service\Server', 'servicemap', function (\DragonJsonServer\Event\Servicemap $servicemap) {
+    		$securitytokens = $servicemap->getServiceManager()->get('Config')['securitytokens'];
+	        foreach ($servicemap->getServicemap()->getServices() as $method => $service) {
+	        	foreach ($securitytokens as $namespace => $securitytoken) {
+	    			$namespace .= '.';
+	        		if (substr($method, 0, strlen($namespace)) != $namespace) {
+	        			continue;
+	        		}
+		            $service->addParams([
+		                [
+		                    'type' => 'string',
+		                    'name' => 'securitytoken',
+		                    'optional' => false,
+		                ],
+		            ]);
+	        	}
+	        }
+    	}]
+    ],
+];
