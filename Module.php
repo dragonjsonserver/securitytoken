379b8ecf6cb2d0bb4651e5b11cc9edec8ce194bf
Christoph Herrmann <developer@dragonprojects.de>
2013-04-06 00:35:05 +0200

    Die Definition der Eventlistener in die "init()" der Modulklasse verschoben

--- Module.php
+++ Module.php
@@ -37,4 +37,47 @@ class Module
             ],
         ];
     }
+    
+    /**
+     * Wird bei der Initialisierung des Moduls aufgerufen
+     * @param \Zend\ModuleManager\ModuleManager $moduleManager
+     */
+    public function init(\Zend\ModuleManager\ModuleManager $moduleManager)
+    {
+    	$sharedManager = $moduleManager->getEventManager()->getSharedManager();
+    	$sharedManager->attach('DragonJsonServer\Service\Server', 'request', 
+    		function (\DragonJsonServer\Event\Request $request) {
+    			$method = $request->getRequest()->getMethod();
+    			foreach ($request->getServiceManager()->get('Config')['securitytokens'] as $namespace => $securitytoken) {
+    				$namespace .= '.';
+    				if (substr($method, 0, strlen($namespace)) != $namespace) {
+    					continue;
+    				}
+    				if ($request->getRequest()->getParam('securitytoken') != $securitytoken) {
+    					throw new \DragonJsonServer\Exception('incorrect securitytoken', ['namespace' => $namespace]);
+    				}
+    			}
+    		}		
+    	);
+    	$sharedManager->attach('DragonJsonServer\Service\Server', 'servicemap', 
+    		function (\DragonJsonServer\Event\Servicemap $servicemap) {
+	    		$securitytokens = $servicemap->getServiceManager()->get('Config')['securitytokens'];
+		        foreach ($servicemap->getServicemap()->getServices() as $method => $service) {
+		        	foreach ($securitytokens as $namespace => $securitytoken) {
+		    			$namespace .= '.';
+		        		if (substr($method, 0, strlen($namespace)) != $namespace) {
+		        			continue;
+		        		}
+			            $service->addParams([
+			                [
+			                    'type' => 'string',
+			                    'name' => 'securitytoken',
+			                    'optional' => false,
+			                ],
+			            ]);
+		        	}
+		        }
+    		}
+	    );
+    }
 }
--- config/module.config.php
+++ config/module.config.php
@@ -12,36 +12,4 @@
  */
 return [
     'securitytokens' => [],
-    'eventlisteners' => [
-    	['DragonJsonServer\Service\Server', 'request', function (\DragonJsonServer\Event\Request $request) {
-    		$method = $request->getRequest()->getMethod();
-    		foreach ($request->getServiceManager()->get('Config')['securitytokens'] as $namespace => $securitytoken) {
-    			$namespace .= '.';
-	    		if (substr($method, 0, strlen($namespace)) != $namespace) {
-	    			continue;
-	    		}
-	    		if ($request->getRequest()->getParam('securitytoken') != $securitytoken) {
-	    			throw new \DragonJsonServer\Exception('incorrect securitytoken', ['namespace' => $namespace]);
-	    		} 
-    		}
-    	}],
-    	['DragonJsonServer\Service\Server', 'servicemap', function (\DragonJsonServer\Event\Servicemap $servicemap) {
-    		$securitytokens = $servicemap->getServiceManager()->get('Config')['securitytokens'];
-	        foreach ($servicemap->getServicemap()->getServices() as $method => $service) {
-	        	foreach ($securitytokens as $namespace => $securitytoken) {
-	    			$namespace .= '.';
-	        		if (substr($method, 0, strlen($namespace)) != $namespace) {
-	        			continue;
-	        		}
-		            $service->addParams([
-		                [
-		                    'type' => 'string',
-		                    'name' => 'securitytoken',
-		                    'optional' => false,
-		                ],
-		            ]);
-	        	}
-	        }
-    	}],
-    ],
 ];
