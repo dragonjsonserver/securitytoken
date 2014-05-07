459d7f2dbfc2c7c9c0de9b7867c653a9dcaa1d1c
Christoph Herrmann <developer@dragonprojects.de>
2014-05-07 18:42:12 +0200

    Securitytokens gehören in die lokale Konfiguration

---
+++ config/local.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @link http://dragonjsonserver.de/
+ * @copyright Copyright (c) 2012-2014 DragonProjects (http://dragonprojects.de/)
+ * @license http://license.dragonprojects.de/dragonjsonserver.txt New BSD License
+ * @author Christoph Herrmann <developer@dragonprojects.de>
+ * @package DragonJsonServerSecuritytoken
+ */
+
+/**
+ * @return array
+ */
+return [
+	'dragonjsonserversecuritytoken' => [
+	    'securitytokens' => [
+	    	'%namespace%' => '%securitytoken%',
+	    	'%namespace%' => '%securitytoken%',
+	    ],
+	],
+];
